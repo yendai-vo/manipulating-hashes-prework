d4a491e680c842f8e21cccaeb668005b612d9e72
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 18:20:56 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -19,4 +19,3 @@ def first_challenge
   #remember to return your newly altered contacts hash!
   contacts
 end
-
