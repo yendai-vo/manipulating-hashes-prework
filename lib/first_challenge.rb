311c3def1214a0a64842d205437624020a523090
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 18:27:47 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -13,10 +13,7 @@ def first_challenge
     }
   }
 
-  #your code here
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if {|value| value == "strawberry"}
 
-
-  #remember to return your newly altered contacts hash!
   contacts
 end
-
